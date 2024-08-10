3f80b04edbea83497696505d279353cccb96656b
Andrew Tang <actang150@gmail.com>
2024-08-10 14:34:30 -0700

    Fix test error msgs

--- list/list_test.go
+++ list/list_test.go
@@ -8,67 +8,67 @@ func TestPushFront(t *testing.T) {
 	e1 := l.PushFront(1)
 
 	if l.Len() != 1 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 1)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 1)
 	}
 
 	if e1.Value != 1 {
-		t.Fatalf("invalid element value. got %d, want %d", e1.Value, 1)
+		t.Fatalf("Invalid element value. got %d, want %d", e1.Value, 1)
 	}
 
 	if e1.Next() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e1.Next(), nil)
+		t.Fatalf("Invalid next element. got %v, want %v", e1.Next(), nil)
 	}
 
 	if e1.Prev() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e1.Prev(), nil)
+		t.Fatalf("Invalid prev element. got %v, want %v", e1.Prev(), nil)
 	}
 
 	e2 := l.PushFront(2)
 
 	if l.Len() != 2 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 2)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 2)
 	}
 
 	if e2.Value != 2 {
-		t.Fatalf("invalid element value. got %d, want %d", e2.Value, 2)
+		t.Fatalf("Invalid element value. got %d, want %d", e2.Value, 2)
 	}
 
 	if e2.Next() != e1 {
-		t.Fatalf("invalid next element. got %v, want %v", e2.Next(), e1)
+		t.Fatalf("Invalid next element. got %v, want %v", e2.Next(), e1)
 	}
 
 	if e2.Next().Next() != nil {
-		t.Fatalf("invalid next next element. got %v, want %v", e2.Next().Next(), nil)
+		t.Fatalf("Invalid next next element. got %v, want %v", e2.Next().Next(), nil)
 	}
 
 	if e2.Prev() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e2.Prev(), nil)
+		t.Fatalf("Invalid prev element. got %v, want %v", e2.Prev(), nil)
 	}
 
 	e3 := l.PushFront(3)
 
 	if l.Len() != 3 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 3)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 3)
 	}
 
 	if e3.Value != 3 {
-		t.Fatalf("invalid element value. got %d, want %d", e3.Value, 3)
+		t.Fatalf("Invalid element value. got %d, want %d", e3.Value, 3)
 	}
 
 	if e3.Next() != e2 {
-		t.Fatalf("invalid next element. got %v, want %v", e3.Next(), e2)
+		t.Fatalf("Invalid next element. got %v, want %v", e3.Next(), e2)
 	}
 
 	if e3.Next().Next() != e1 {
-		t.Fatalf("invalid next next element. got %v, want %v", e3.Next().Next(), e1)
+		t.Fatalf("Invalid next next element. got %v, want %v", e3.Next().Next(), e1)
 	}
 
 	if e3.Next().Next().Next() != nil {
-		t.Fatalf("invalid next next element. got %v, want %v", e3.Next().Next().Next(), nil)
+		t.Fatalf("Invalid next next next element. got %v, want %v", e3.Next().Next().Next(), nil)
 	}
 
 	if e3.Prev() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e3.Prev(), nil)
+		t.Fatalf("Invalid prev element. got %v, want %v", e3.Prev(), nil)
 	}
 }
 
@@ -78,67 +78,67 @@ func TestPushBack(t *testing.T) {
 	e1 := l.PushBack(1)
 
 	if l.Len() != 1 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 1)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 1)
 	}
 
 	if e1.Value != 1 {
-		t.Fatalf("invalid element value. got %d, want %d", e1.Value, 1)
-	}
-
-	if e1.Next() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e1.Next(), nil)
+		t.Fatalf("Invalid element value. got %d, want %d", e1.Value, 1)
 	}
 
 	if e1.Prev() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e1.Prev(), nil)
+		t.Fatalf("Invalid prev element. got %v, want %v", e1.Prev(), nil)
+	}
+
+	if e1.Next() != nil {
+		t.Fatalf("Invalid next element. got %v, want %v", e1.Next(), nil)
 	}
 
 	e2 := l.PushBack(2)
 
 	if l.Len() != 2 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 2)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 2)
 	}
 
 	if e2.Value != 2 {
-		t.Fatalf("invalid element value. got %d, want %d", e2.Value, 2)
+		t.Fatalf("Invalid element value. got %d, want %d", e2.Value, 2)
 	}
 
 	if e2.Prev() != e1 {
-		t.Fatalf("invalid next element. got %v, want %v", e2.Prev(), e1)
+		t.Fatalf("Invalid prev element. got %v, want %v", e2.Prev(), e1)
 	}
 
 	if e2.Prev().Prev() != nil {
-		t.Fatalf("invalid next next element. got %v, want %v", e2.Prev().Prev(), nil)
+		t.Fatalf("Invalid prev prev element. got %v, want %v", e2.Prev().Prev(), nil)
 	}
 
 	if e2.Next() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e2.Next(), nil)
+		t.Fatalf("Invalid next element. got %v, want %v", e2.Next(), nil)
 	}
 
 	e3 := l.PushBack(3)
 
 	if l.Len() != 3 {
-		t.Fatalf("invalid list size. got %d, want %d", l.Len(), 3)
+		t.Fatalf("Invalid list size. got %d, want %d", l.Len(), 3)
 	}
 
 	if e3.Value != 3 {
-		t.Fatalf("invalid element value. got %d, want %d", e3.Value, 3)
+		t.Fatalf("Invalid element value. got %d, want %d", e3.Value, 3)
 	}
 
 	if e3.Prev() != e2 {
-		t.Fatalf("invalid next element. got %v, want %v", e3.Prev(), e2)
+		t.Fatalf("Invalid prev element. got %v, want %v", e3.Prev(), e2)
 	}
 
 	if e3.Prev().Prev() != e1 {
-		t.Fatalf("invalid next next element. got %v, want %v", e3.Prev().Prev(), e1)
+		t.Fatalf("Invalid prev prev element. got %v, want %v", e3.Prev().Prev(), e1)
 	}
 
 	if e3.Prev().Prev().Prev() != nil {
-		t.Fatalf("invalid next next element. got %v, want %v", e3.Prev().Prev().Prev(), nil)
+		t.Fatalf("Invalid prev prev prev element. got %v, want %v", e3.Prev().Prev().Prev(), nil)
 	}
 
 	if e3.Next() != nil {
-		t.Fatalf("invalid next element. got %v, want %v", e3.Next(), nil)
+		t.Fatalf("Invalid next element. got %v, want %v", e3.Next(), nil)
 	}
 }
 
@@ -168,12 +168,12 @@ func TestBack(t *testing.T) {
 	e := l.PushBack(3)
 
 	if l.Back() != e {
-		t.Fatalf("Invalid front element. got %v, want %v", l.Back(), e)
+		t.Fatalf("Invalid back element. got %v, want %v", l.Back(), e)
 	}
 
 	l.PushFront(4)
 
 	if l.Back() != e {
-		t.Fatalf("Invalid front element. got %v, want %v", l.Back(), e)
+		t.Fatalf("Invalid back element. got %v, want %v", l.Back(), e)
 	}
 }
